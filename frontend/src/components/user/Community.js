495042079990a5317a79a8458ccdffb8fd508b03
MyDXD <dislove107@gmail.com>
2024-01-25 23:17:30 +0700

    แก้ หน้าหลัก และ ชุมชน

--- frontend/src/components/user/Community.js
+++ frontend/src/components/user/Community.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Community() {
   return (
-    <div>Community</div>
+    <div class="flex mx-72 my-10">
+      <div class="flex-auto w-2/3 border-4 border-purple-400 p-20 m-1 bg-slate-200">
+        test
+      </div>
+      <div class="flex-auto w-1/3 border-4 border-purple-400 p-20  m-1">
+        
+      </div>
+    </div>
   )
 }
