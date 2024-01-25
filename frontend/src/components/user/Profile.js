dce8c3e71c63f384042b122c66b31b8b14aded89
Don_jakkapat <79179631+jakkapatintarat@users.noreply.github.com>
2024-01-26 02:38:30 +0700

    Update Profile.js

--- frontend/src/components/user/Profile.js
+++ frontend/src/components/user/Profile.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Profile() {
+  const [imageUpload, setImageUpload] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData;
+      formData.append('photo', imageUpload);
+      console.log(imageUpload);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handlePhoto = (e) => {
+    setImageUpload(e.target.files[0]);
+  };
+
   return (
-    <div>Profile</div>
+    <>
+      <div>Profile</div>
+      <div>
+      <h2>Upload Image</h2>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <input type="file" accept=".png, .jpg, .jpeg" name="photo" onChange={handlePhoto}/>
+        <button type="submit">Upload</button>
+      </form>
+    </div>
+
+    </>
   )
 }
