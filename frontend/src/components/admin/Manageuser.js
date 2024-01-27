f9602e4d8f1035eadbb1a8dd76b1c73a46e9a68a
MyDXD <dislove107@gmail.com>
2024-01-28 03:02:13 +0700

    Create user admin // ใช้ register ของ user

--- frontend/src/components/admin/Manageuser.js
+++ frontend/src/components/admin/Manageuser.js
@@ -1,14 +1,52 @@
 import axios from 'axios';
 import React, { useEffect, useState, Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
-import { CheckIcon } from '@heroicons/react/24/outline'
+
+
+
+
 
 export default function Manageuser() {
   const [users, setUsers] = useState([]);
   const [open, setOpen] = useState(false)
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [telephone, setTelephone] = useState('');
+
+
+
+const handleCreate = async (event) => {
+  event.preventDefault();
+  console.log(username);
+  console.log(password);
+  console.log(firstName);
+  console.log(lastName);
+  console.log(telephone);
+  console.log(email);
+  try {
+      const response = await axios.post("http://localhost:5000/api/register", {
+          username: username,
+          password: password,
+          fname: firstName,
+          lname: lastName,
+          email: email,
+      });
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      setOpen(false)
+  } catch (error) {
+      console.error("Sign in failed", error);
+  }
+};
   useEffect(() => {
     axios.get('http://localhost:5000/api/users')
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        console.log(res.data);
+        setUsers(res.data)
+      })
       .catch((err) => {
         console.error(err);
       });
@@ -67,30 +105,146 @@ export default function Manageuser() {
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                       >
                         <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
-                          <div>
-                            <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                              <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-                            </div>
-                            <div className="mt-3 text-center sm:mt-5">
-                              <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                                Payment successful
-                              </Dialog.Title>
-                              <div className="mt-2">
-                                <p className="text-sm text-gray-500">
-                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="mt-5 sm:mt-6">
-                            <button
-                              type="button"
-                              className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                              onClick={() => setOpen(false)}
+                        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" onSubmit={handleCreate} >
+                        <div>
+                            <label
+                                htmlFor="username"
+                                className="block text-sm font-medium leading-6 text-gray-900"
                             >
-                              Go back to dashboard
+                                username
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="username"
+                                    name="username"
+                                    type="text"
+                                    autoComplete="username"
+                                    required
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label
+                                    htmlFor="password"
+                                    className="block text-sm font-medium leading-6 text-gray-900"
+                                >
+                                    Password
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label
+                                    htmlFor="firstname"
+                                    className="block text-sm font-medium leading-6 text-gray-900"
+                                >
+                                    ชื่อ
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="firstname"
+                                    name="firstname"
+                                    type="text"
+                                    autoComplete="current-password"
+                                    required
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label
+                                    htmlFor="lastname"
+                                    className="block text-sm font-medium leading-6 text-gray-900"
+                                >
+                                    นามสกุล
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="lastname"
+                                    name="lastname"
+                                    type="text"
+                                    autoComplete="current-password"
+                                    required
+                                    onChange={(e) => setLastName(e.target.value)}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label
+                                    htmlFor="telephone"
+                                    className="block text-sm font-medium leading-6 text-gray-900"
+                                >
+                                    เบอร์โทรศัพท์
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="telephone"
+                                    name="telephone"
+                                    type="text"
+                                    autoComplete="current-password"
+                                    required
+                                    onChange={(e) => setTelephone(e.target.value)}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label
+                                    htmlFor="email"
+                                    className="block text-sm font-medium leading-6 text-gray-900"
+                                >
+                                E-mail
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="current-password"
+                                    required
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                เพิ่มผู้ใช้
                             </button>
-                          </div>
+                        </div>
+                    </form>
+
+                </div>
                         </Dialog.Panel>
                       </Transition.Child>
                     </div>
@@ -131,7 +285,7 @@ export default function Manageuser() {
                           <td className="darkspace-nowrap px-3 py-4 text-sm text-dark-900">{user.email}</td>
                           <td className="darkspace-nowrap px-3 py-4 text-sm text-dark-900">{user.fname}</td>
                           <td className="relative darkspace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                            <a href="#" className="text-indigo-400 hover:text-indigo-300">
+                            <a href="/admin/manageuser" className="text-indigo-400 hover:text-indigo-300">
                               Edit<span className="sr-only">, {user.fname}</span>
                             </a>
                           </td>
