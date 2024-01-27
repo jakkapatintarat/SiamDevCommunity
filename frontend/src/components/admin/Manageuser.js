ad737aa9fee7020bb162c67fcd840ff595e1fedf
MyDXD <dislove107@gmail.com>
2024-01-28 02:44:22 +0700

    จัดการผู้ใช้ admin และ มี Profile user

--- frontend/src/components/admin/Manageuser.js
+++ frontend/src/components/admin/Manageuser.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { CheckIcon } from '@heroicons/react/24/outline'
 
 export default function Manageuser() {
   const [users, setUsers] = useState([]);
-
+  const [open, setOpen] = useState(false)
   useEffect(() => {
     axios.get('http://localhost:5000/api/users')
       .then((res) => setUsers(res.data))
@@ -14,75 +16,137 @@ export default function Manageuser() {
 
   return (
     <main className="pt-0 pl-72 ">
+
       <div>
 
         {/* Table */}
         <div className="bg-gray-200">
-          
-            <div className="px-4 sm:px-6 lg:px-8">
-              <div className="sm:flex sm:items-center">
-                <div className="sm:flex-auto mt-10">
-                  <h1 className="text-base font-semibold leading-6 text-dark">Manage Users</h1>
-                  <p className="mt-2 text-sm text-gray-300">
-                    A list of all the users in your account including their name, title, email and role.
-                  </p>
-                </div>
-                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                  <button
-                    type="button"
-                    className="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-dark hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                  >
-                    Add user
-                  </button>
-                </div>
+
+          <div className="px-4 sm:px-6 lg:px-8">
+            <div className="sm:flex sm:items-center">
+              <div className="sm:flex-auto mt-10">
+                <h1 className="text-base font-semibold leading-6 text-dark">Manage Users</h1>
+                <p className="mt-2 text-sm text-gray-300">
+                  A list of all the users in your account including their name, title, email and role.
+                </p>
               </div>
-              <div className="mt-8 flow-root">
-                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                  <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                    <table className="min-w-full divide-y divide-gray-700">
-                      <thead>
-                        <tr>
-                          <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-dark sm:pl-0">
-                            Name
-                          </th>
-                          <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-dark">
-                            Title
-                          </th>
-                          <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-dark">
-                            Email
-                          </th>
-                          <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-dark">
-                            Role
-                          </th>
-                          <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                            <span className="sr-only">Edit</span>
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody className="divide-y divide-gray-300">
-                        {users.map((user) => (
-                          <tr key={user._id}>
-                            <td className="darkspace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-dark sm:pl-0">
-                              {user.username}
-                            </td>
-                            <td className="darkspace-nowrap px-3 py-4 text-sm text-dark-900">{user.password}</td>
-                            <td className="darkspace-nowrap px-3 py-4 text-sm text-dark-900">{user.email}</td>
-                            <td className="darkspace-nowrap px-3 py-4 text-sm text-dark-900">{user.fname}</td>
-                            <td className="relative darkspace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                              <a href="#" className="text-indigo-400 hover:text-indigo-300">
-                                Edit<span className="sr-only">, {user.fname}</span>
-                              </a>
-                            </td>
-                          </tr>
-                        ))}
-                      </tbody>
-                    </table>
+
+
+            </div>
+            <div name={"modol"}>
+              <button
+                type="button"
+                onClick={() => setOpen(true)}
+                className="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-dark hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Add user
+              </button>
+              <Transition.Root show={open} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={setOpen}>
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                  </Transition.Child>
+
+                  <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                      <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enterTo="opacity-100 translate-y-0 sm:scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                      >
+                        <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                          <div>
+                            <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                              <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                            </div>
+                            <div className="mt-3 text-center sm:mt-5">
+                              <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                                Payment successful
+                              </Dialog.Title>
+                              <div className="mt-2">
+                                <p className="text-sm text-gray-500">
+                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="mt-5 sm:mt-6">
+                            <button
+                              type="button"
+                              className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                              onClick={() => setOpen(false)}
+                            >
+                              Go back to dashboard
+                            </button>
+                          </div>
+                        </Dialog.Panel>
+                      </Transition.Child>
+                    </div>
                   </div>
+                </Dialog>
+              </Transition.Root>
+            </div>
+            <div className="mt-8 flow-root">
+              <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                  <table className="min-w-full divide-y divide-gray-700">
+                    <thead>
+                      <tr>
+                        <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-dark sm:pl-0">
+                          Name
+                        </th>
+                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-dark">
+                          Title
+                        </th>
+                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-dark">
+                          Email
+                        </th>
+                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-dark">
+                          Role
+                        </th>
+                        <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                          <span className="sr-only">Edit</span>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-300">
+                      {users.map((user) => (
+                        <tr key={user._id}>
+                          <td className="darkspace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-dark sm:pl-0">
+                            {user.username}
+                          </td>
+                          <td className="darkspace-nowrap px-3 py-4 text-sm text-dark-900">{user.password}</td>
+                          <td className="darkspace-nowrap px-3 py-4 text-sm text-dark-900">{user.email}</td>
+                          <td className="darkspace-nowrap px-3 py-4 text-sm text-dark-900">{user.fname}</td>
+                          <td className="relative darkspace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                            <a href="#" className="text-indigo-400 hover:text-indigo-300">
+                              Edit<span className="sr-only">, {user.fname}</span>
+                            </a>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
                 </div>
               </div>
             </div>
+          </div>
         </div>
         {/* End Table */}
+
+
       </div>
     </main>
   )
--- frontend/src/components/user/Profile.js
+++ frontend/src/components/user/Profile.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react'
 
 export default function Profile() {
-  const [imageUpload, setImageUpload] = useState(null);
-
-
   const userData = localStorage.getItem("token")
   const decodedPayload = JSON.parse(atob(userData.split(".")[1]));
   const profile = decodedPayload.result || decodedPayload;
-  // console.log(profile);
+  console.log(profile);
   // const handleSubmit = async (e) => {
   //   e.preventDefault();
   //   try {
@@ -19,9 +16,6 @@ export default function Profile() {
   //   }
   // }
 
-  const handlePhoto = (e) => {
-    setImageUpload(e.target.files[0]);
-  };
 
   return (
     <div className="p-36">
@@ -35,50 +29,38 @@ export default function Profile() {
             />
             <div className="py-2">
               <h3 className="font-bold text-2xl text-gray-800 dark:text-white mb-1">{profile.username}</h3>
+              <h3 className="font-bold text-2xl text-gray-400 mb-1"></h3>
+
               <div className="inline-flex text-gray-700 dark:text-gray-300 items-center">
-                <svg
-                  className="h-5 w-5 text-gray-400 dark:text-gray-600 mr-1"
-                  fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                >
-                  <path
-                    className=""
-                    d="M5.64 16.36a9 9 0 1 1 12.72 0l-5.65 5.66a1 1 0 0 1-1.42 0l-5.65-5.66zm11.31-1.41a7 7 0 1 0-9.9 0L12 19.9l4.95-4.95zM12 14a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
-                  />
-                </svg>
-                {profile.tel}
               </div>
             </div>
           </div>
-          <div className="flex gap-2 px-2">
-            <button className="flex-1 rounded-full bg-blue-600 dark:bg-blue-800 text-white dark:text-white antialiased font-bold hover:bg-blue-800 dark:hover:bg-blue-900 px-4 py-2">
-              Follow
-            </button>
-            <button className="flex-1 rounded-full border-2 border-gray-400 dark:border-gray-700 font-semibold text-black dark:text-white px-4 py-2">
-              Message
-            </button>
+
+        </div>
+        <div className="px-4 pt-4">
+          <div className="flex gap-2 items-center text-gray-800 dark:text-gray-300 mb-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25" />
+            </svg>
+
+            <span>
+              <h1 className="text-black dark:text-white">E-mail: {profile.email}</h1>
+            </span>
+          </div>
+          <div className="flex">
+            <div className="flex justify-end mr-2">
+
+            </div>
           </div>
         </div>
-        <div className="px-4 py-4">
+        <div className="px-4 py-0">
           <div className="flex gap-2 items-center text-gray-800 dark:text-gray-300 mb-4">
-            <svg
-              className="h-6 w-6 text-gray-600 dark:text-gray-400"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-            >
-              <path
-                className=""
-                d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"
-              />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
             </svg>
+
             <span>
-              <strong className="text-black dark:text-white">12</strong> Followers you know
+              <h1 className="text-black dark:text-white">ชื่อ: {profile.fname} นามสกุล: {profile.lname}</h1>
             </span>
           </div>
           <div className="flex">
