41aca448feaea8e0cf7ee3a1aa65902c138951f2
MyDXD <dislove107@gmail.com>
2024-01-28 00:16:10 +0700

    admin login

--- frontend/src/routes/Adminroutes.js
+++ frontend/src/routes/Adminroutes.js
@@ -3,12 +3,14 @@ import Dashboard from "../components/admin/Dashboard"
 import Sidebar from "../components/admin/Sidebar"
 import Manageuser from "../components/admin/Manageuser";
 import Manageblog from "../components/admin/Manageblog";
+import LoginAdmin from "../components/admin/LoginAdmin";
 export default function AdminRoutes() {
     return (
         <>
         <Routes>
             {/* Admin Routes */}
             <Route path='/admin' element={<><Sidebar/><Dashboard/></>} />
+            <Route path='/admin/login' element={<><LoginAdmin/></>} />
             <Route path='/admin/manageuser' element={<><Sidebar/><Manageuser/></>} />
             <Route path='/admin/manageblog' element={<><Sidebar/><Manageblog/></>} />
         </Routes>
