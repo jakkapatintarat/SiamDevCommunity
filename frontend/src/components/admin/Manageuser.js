52ae9585ef35a6615658be733193a2b833421e04
Don_jakkapat <79179631+jakkapatintarat@users.noreply.github.com>
2024-01-27 23:32:43 +0700

    หน้าจัดการ user blogs

--- frontend/src/components/admin/Manageuser.js
+++ frontend/src/components/admin/Manageuser.js
@@ -1,10 +1,91 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 export default function Manageuser() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(async () => {
+    await axios.get('http://localhost:5000/api/users')
+    .then((res) => setUsers(res.data))
+    .catch((err) => {
+      console.error(err);
+    });
+  }, []);
+
   return (
     <main className="py-10 lg:pl-72">
       <div className="px-4 sm:px-6 lg:px-8">
-        <h1>Manageuser</h1>
+      
+        {/* Table */}
+        <div className="bg-gray-900">
+          <div className="mx-auto max-w-7xl">
+            <div className="bg-gray-900 py-10">
+              <div className="px-4 sm:px-6 lg:px-8">
+                <div className="sm:flex sm:items-center">
+                  <div className="sm:flex-auto">
+                    <h1 className="text-base font-semibold leading-6 text-white">Manage Users</h1>
+                    <p className="mt-2 text-sm text-gray-300">
+                      A list of all the users in your account including their name, title, email and role.
+                    </p>
+                  </div>
+                  <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <button
+                      type="button"
+                      className="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    >
+                      Add user
+                    </button>
+                  </div>
+                </div>
+                <div className="mt-8 flow-root">
+                  <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                      <table className="min-w-full divide-y divide-gray-700">
+                        <thead>
+                          <tr>
+                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">
+                              Name
+                            </th>
+                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-white">
+                              Title
+                            </th>
+                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-white">
+                              Email
+                            </th>
+                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-white">
+                              Role
+                            </th>
+                            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                              <span className="sr-only">Edit</span>
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-800">
+                          {users.map((user) => (
+                            <tr key={user._id}>
+                              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
+                                {user.username}
+                              </td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{user.password}</td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{user.email}</td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{user.fname}</td>
+                              <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                                <a href="#" className="text-indigo-400 hover:text-indigo-300">
+                                  Edit<span className="sr-only">, {user.fname}</span>
+                                </a>
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* End Table */}
       </div>
     </main>
   )
