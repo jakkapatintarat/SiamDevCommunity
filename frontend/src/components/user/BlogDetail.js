7e1474c490474301984d3b4908a612e60c676f94
MyDXD <dislove107@gmail.com>
2024-02-08 00:00:17 +0700

    ปรับ layout หน้า blog และ blogDetail

--- frontend/src/components/user/BlogDetail.js
+++ frontend/src/components/user/BlogDetail.js
@@ -22,57 +22,18 @@ export default function BlogDetail() {
   }, []);
 
   return (
-    <div className="container-full bg-slate-700 flex justify-center items-center overflow">
-      <div className="container mx-auto bg-slate-600 p-8 sm:p-12 h-screen sm:h-auto rounded-md ">
-        
-        <p className="text-4xl text-center mb-2">{blog.title}</p>
-        <p className=" text-center mb-8">By: {blog.author}</p>
+    <div className="bg-slate-700 min-h-screen flex justify-center ">
+      <div className="bg-slate-600 w-full mx-20 p-8 ">
+        <h1 className="text-center text-gray-50 text-3xl">Title : {blog.title}</h1>
+        <h1 className="text-center text-gray-50 text-xl text-bg-dark">By : {blog.author}</h1>
 
-        <div className="flex justify-center mb-8">
-          <img
-            src={blog.img}
-            alt=""
-            className="rounded-2xl w-full sm:w-1/3 object-fill lg:aspect-auto lg:h-[25.5rem]"
-          />
+        <div className="p-4 rounded-md flex justify-center items-center">
+          <div><img src={blog.img} className=" h-auto max-w-full rounded-lg"/></div>
         </div>
 
-        <div className="bg-white text-xl  rounded-md p-5 overflow-auto">
-          <p className="">{blog.content}กฟหกฟหกหฟกหฟกฟหกกฟห</p>
-          {/* Your long content here */}Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Esse eaque veritatis, doloremque rerum quis,
-          assumenda et consectetur asperiores, reprehenderit tenetur repudiandae
-          totam. Iure architecto, est porro ad aliquam mollitia magni hic quod
-          culpa quasi praesentium cum optio nemo quibusdam, modi laboriosam.
-          Repudiandae ratione eos delectus soluta, sequi nulla quas quisquam?
-          Repudiandae aspernatur quis atque reprehenderit blanditiis
-          perspiciatis rerum aliquid doloribus est dolore dicta vero quidem,
-          fuga aperiam ex velit iusto deserunt nam animi non, quibusdam magni
-          accusamus a saepe? Tenetur, soluta! Ea iste aut nihil doloribus
-          maiores quo qui ipsa ad exercitationem cum! Quidem commodi pariatur
-          facere tempore ducimus explicabo fuga perspiciatis, excepturi eligendi
-          veniam consectetur doloribus eveniet dicta. Porro eveniet quos maiores
-          consectetur accusantium illo quia, voluptatum itaque ratione
-          temporibus expedita necessitatibus excepturi debitis explicabo laborum
-          minus quisquam sint quaerat dignissimos unde officiis deserunt maxime.
-          Ullam rem nemo architecto voluptates suscipit error, fugit ad culpa
-          quidem repellat porro totam iure molestias. Blanditiis id voluptatum
-          laborum iusto beatae aliquid ratione eligendi doloribus reiciendis
-          expedita tenetur ab consequuntur recusandae, eveniet quia, ea cum
-          molestias perferendis sequi in nesciunt repellendus, obcaecati
-          aperiam. Repellendus ex ea porro tenetur maxime officiis impedit
-          voluptatum nostrum, magnam vero ad ratione eius sit alias, deserunt
-          iure cupiditate culpa molestiae assumenda consequatur maiores officia
-          dolore nulla provident? Corrupti provident eaque reprehenderit aut
-          inventore eos quasi expedita cumque! Sint eius vel explicabo autem
-          inventore sunt, est doloribus beatae nesciunt quia dolorem delectus
-          quidem molestias enim omnis aperiam suscipit voluptatem laborum nulla
-          tempore nisi! Molestiae vel quidem corporis delectus minima,
-          exercitationem maxime fugiat quis dolores saepe nemo voluptatem nihil
-          sunt error aspernatur itaque pariatur iusto voluptate recusandae
-          ullam. Architecto velit nostrum illo error cupiditate ea ipsam
-          expedita voluptatibus cumque dignissimos sed porro quam qui
-          consequuntur, voluptatem odit quasi ratione tempore rerum animi
-          praesentium enim beatae quos sint. Nemo, beatae placeat.
+        <div className="bg-white p-4 mb-4 rounded-md mx-20">
+          {blog.content}
+    
         </div>
       </div>
     </div>
--- frontend/src/components/user/Blogs.js
+++ frontend/src/components/user/Blogs.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Blogs() {
   const [blogs, setBlogs] = useState([]);
@@ -7,10 +7,10 @@ export default function Blogs() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/blogs');
+        const response = await axios.get("http://localhost:5000/api/blogs");
         setBlogs(response.data);
       } catch (error) {
-        console.error('Error fetching blogs:', error);
+        console.error("Error fetching blogs:", error);
       }
     };
 
@@ -20,25 +20,53 @@ export default function Blogs() {
   return (
     <>
       <div className="bg-white">
-
-        <form className='m-16 mb-2'>
-          <div class="relative">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-              </svg>
-            </div>
-            <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-            <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8">
+          <div>
+            <form className="mb-10">
+              <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <svg
+                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  type="search"
+                  id="default-search"
+                  class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="ค้นหาบทความ..."
+                  required
+                />
+                <button
+                  type="submit"
+                  class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Search
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
-
-
-
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <div className="md:flex md:items-center md:justify-between">
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900">Trending products</h2>
-            <a href='/createblog' className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">สร้างบทความ</a>
+            <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+              Trending products
+            </h2>
+            <a
+              href="/createblog"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              สร้างบทความ
+            </a>
           </div>
           <div className="mt-6 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8">
             {blogs.map((blog) => (
@@ -58,11 +86,12 @@ export default function Blogs() {
                 </h3>
 
                 <p className="mt-1 text-sm text-gray-500">{blog.author}</p>
-                <p className="mt-1 text-sm font-medium text-gray-900">{blog.content}</p>
+                <p className="mt-1 text-sm font-medium text-gray-900">
+                  {blog.content}
+                </p>
               </div>
             ))}
           </div>
-
         </div>
       </div>
     </>
